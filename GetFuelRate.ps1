54f1613e9bfa6487c42b3f3ff4f24dd595588bac
hoaxuyentri <78299615+hoaxuyentri@users.noreply.github.com>
2021-02-21 16:23:26 +0700

    Add files via upload

---
+++ GetFuelRate.ps1
@@ -0,0 +1,46 @@
+﻿###Define input data
+$FuelType = 'DO 0,05S-II'
+$server = "LAPTOP310\SQLLUANTT"
+$database = "MIN_NPM"
+
+###Get website html content
+$WebResponseObj = Invoke-WebRequest -Uri "https://www.petrolimex.com.vn" -UseBasicParsing
+$Content = $WebResponseObj.Content
+
+###Define input parameters for store procedure
+$String = $Content.Substring($Content.IndexOf($FuelType), $FuelType.Length + 38)
+$Zone1 = ($String.Substring($FuelType.Length + 15, 6)).Replace(".","")
+$Zone2 = ($String.Substring($String.Length - 6, 6)).Replace(".","")
+#Write-Output $Zone1
+#Write-Output $Zone2
+
+$MFirstDate = (Get-date).AddDays(1-(Get-date).Day).ToString("yyyy-MM-dd")
+$MLastDate = (((Get-date).AddDays(1-(Get-date).Day)).AddMonths(1)).AddDays(-1).ToString("yyyy-MM-dd")
+#Write-Output $MFirstDate
+#Write-Output $MLastDate
+
+###Connect to server and execute store procedure
+$scon = New-Object System.Data.SqlClient.SqlConnection
+$scon.ConnectionString = "Data Source=$server;Initial Catalog=$database;Integrated Security=true"
+        
+$cmd = New-Object System.Data.SqlClient.SqlCommand
+$cmd.Connection = $scon
+$cmd.CommandTimeout = 0
+
+### Tie parameters using SqlParameters
+$cmd.CommandText = "DECLARE @ID INT EXEC PP_UI_UPDATE_FUELRATE 1,'$MFirstDate','$MLastDate',$Zone1,$Zone2,@ID"
+
+try
+ {
+   $scon.Open()
+   $cmd.ExecuteNonQuery() | Out-Null
+ }
+catch [Exception]
+ {
+   Write-Warning $_.Exception.Message
+ }
+finally
+ {
+   $scon.Dispose()
+   $cmd.Dispose()
+ }
---
+++ GetFuelRatePicture.ps1
@@ -0,0 +1,73 @@
+﻿$Hide = 0
+$Normal = 1
+$Minimized = 2
+$Maximized = 3
+$ShowNoActivateRecentPosition = 4
+$Show = 5
+$MinimizeActivateNext = 6
+$MinimizeNoActivate = 7
+$ShowNoActivate = 8
+$Restore = 9
+$ShowDefault = 10
+$ForceMinimize = 11
+
+#Specify an interwebs address
+$URL="https://www.petrolimex.com.vn/"
+
+#Create internetexplorer.application object
+$IE=new-object -com "InternetExplorer.Application"
+
+#Set some parameters for the internetexplorer.application object
+$IE.TheaterMode = $False
+$IE.AddressBar = $True
+$IE.StatusBar = $False
+$IE.MenuBar = $True
+$IE.FullScreen = $False
+$IE.visible = $True
+
+#Navigate to the URL
+$IE.navigate2($URL,$null,$true)
+
+#the C#-style signature of an API function
+$code = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+
+#add signature as new type to PowerShell (for this session)
+$type = Add-Type -MemberDefinition $code -Name myAPI -PassThru
+
+#Magic:
+$type::ShowWindowAsync($IE.HWND[0], $Maximized) | Out-Null
+
+do {sleep 5} until (-not ($IE.Busy))
+
+# Take A ScreenShot
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+	$bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+	$graphics = [Drawing.Graphics]::FromImage($bmp)
+
+	$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+	$bmp.Save($path)
+
+	$graphics.Dispose()
+	$bmp.Dispose()
+}
+
+$path = "C:\Luan data\Fuel price " + (Get-date).ToString("yyyy-MM-dd HHmmss") + ".jpg"
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, 1360, 770)
+screenshot $bounds $path
+
+$shellapp = New-Object -ComObject "Shell.Application"
+$ShellWindows = $shellapp.Windows()
+for ($i = 0; $i -lt $ShellWindows.Count; $i++)
+{
+ if ($ShellWindows.Item($i).FullName -like "*iexplore.exe")
+  {
+  	$ie = $ShellWindows.Item($i)
+  	$ie.quit()
+  	[System.Runtime.Interopservices.Marshal]::ReleaseComObject($ie) | Out-Null
+  	[System.GC]::Collect()
+  	[System.GC]::WaitForPendingFinalizers()
+  }
+}
